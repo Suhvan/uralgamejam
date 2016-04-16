7ed9dccfb0cb88835432261fd93c5a1f70fb4563
Suhvan <soohvan@gmail.com>
2016-04-17 04:30:34 +0500

    Физон танкам и спавнер

--- NewOne/Assets/Src/TankFactory.cs
+++ NewOne/Assets/Src/TankFactory.cs
@@ -7,7 +7,15 @@ public class TankFactory : MonoBehaviour {
 	GameObject tankPrefab;
 
 	[SerializeField]
-	float spawnCooldown;
+	float spawnCooldown = 20;
+	float spawnTimer = 0;
+
+	[SerializeField]
+	Transform leftBase;
+
+	[SerializeField]
+	Transform rightBase;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +24,18 @@ public class TankFactory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		spawnTimer += Time.deltaTime;
+		if (spawnTimer > spawnCooldown)
+		{
+			spawnTimer = 0;
+			SpawnTank();
+        }
+	}
+
+	void SpawnTank()
+	{
+		var pos = rightBase.transform.position;
+		pos.z = 0;
+		Instantiate(tankPrefab, pos, Quaternion.identity);
 	}
 }
