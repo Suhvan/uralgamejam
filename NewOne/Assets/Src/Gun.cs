ea9425fdda21f1b840e2f25efc55efa561c9e162
unknown <ivan_suhanov@DUKE.targem.local>
2016-04-16 17:48:31 +0500

    sadasdas

---
+++ NewOne/Assets/Src/Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	private float angle = 0;
+
+
+	public float Angle 
+	{
+		set
+		{ 
+			gameObject.transform.localRotation = Quaternion.Euler (0, 0, value);
+			angle = value;
+		}
+		get
+		{
+			return angle;
+		}
+
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
