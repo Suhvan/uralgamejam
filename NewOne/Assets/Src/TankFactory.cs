885d8debbaba36addf77c3efdbfe066b49397a46
Suhvan <soohvan@gmail.com>
2016-04-17 05:38:43 +0500

    Spawner improvements

--- NewOne/Assets/Src/TankFactory.cs
+++ NewOne/Assets/Src/TankFactory.cs
@@ -11,16 +11,24 @@ public class TankFactory : MonoBehaviour {
 	float spawnTimer = 0;
 
 	[SerializeField]
+	int spawnLimit = 3;
+
+	[SerializeField]
+	int spawnIncStep = 4;
+
+	[SerializeField]
 	Transform leftBase;
 
 	[SerializeField]
 	Transform rightBase;
 
+	int totalSpawned = 1;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+		if (spawnIncStep == 0)
+			spawnIncStep = 1;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -35,7 +43,16 @@ public class TankFactory : MonoBehaviour {
 	void SpawnTank()
 	{
 		var pos = rightBase.transform.position;
+		if (GameCore.Instance.Score > 2 && Random.value < 0.5f)
+		{
+			pos = leftBase.transform.position;
+		}
+
+		if (totalSpawned - GameCore.Instance.Score >= 3 + GameCore.Instance.Score / spawnIncStep)
+			return;
+
 		pos.z = 0;
 		Instantiate(tankPrefab, pos, Quaternion.identity);
-	}
+		totalSpawned++;
+    }
 }
