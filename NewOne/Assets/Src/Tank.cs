66edf589bee1b6c6220c0913baac7b25720863b6
Suhvan <soohvan@gmail.com>
2016-04-17 05:26:39 +0500

    Тайминги поправил

--- NewOne/Assets/Src/Tank.cs
+++ NewOne/Assets/Src/Tank.cs
@@ -32,20 +32,29 @@ public class Tank : Mortal
 	void Update () {
 		if (fireModule!=null && fireModule.Triggered)
 		{
-			fireModule.ReleaseTrigger();
-			var bulletPref = shaftModule.GetBullet();
-			if (bulletPref != null)
-			{
-                if (gunAnim)
-                    gunAnim.SetTrigger("shoot");
-
-				var bullet = (Projectile)Instantiate(bulletPref, shootingPoint.position, shootingPoint.rotation);
-				bullet.Kickstart(0);
-			}
-			else
-			{
-				TopPanel.Instance.setStatus("Нет патронов, чтобы стрелять");				
-			}
+			StartCoroutine(Shoot());
 		}
 	}
+
+	IEnumerator Shoot()
+	{
+		fireModule.ReleaseTrigger();
+		var bulletPref = shaftModule.GetBullet();
+		if (bulletPref != null)
+		{
+			if (gunAnim)
+				gunAnim.SetTrigger("shoot");
+			yield return new WaitForSeconds(0.3f);
+			var bullet = (Projectile)Instantiate(bulletPref, shootingPoint.position, shootingPoint.rotation);
+			bullet.Kickstart(0);
+		}
+		else
+		{
+			TopPanel.Instance.setStatus("Нет патронов, чтобы стрелять");
+		}
+		yield return null;
+	}
+
+
+
 }
