3ef295ebb88efc031aac22b80d2a23c5a7be5d23
Andrew Ishchuk <unlightable@gmail.com>
2016-04-17 06:01:58 +0500

    more aggressive spawns

--- NewOne/Assets/Src/TankFactory.cs
+++ NewOne/Assets/Src/TankFactory.cs
@@ -1,59 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TankFactory : MonoBehaviour {
+public class TankFactory : MonoBehaviour
+{
 
-	[SerializeField]
-	GameObject tankPrefab;
+    [SerializeField]
+    GameObject tankPrefab;
 
-	[SerializeField]
-	float spawnCooldown = 20;
-	float spawnTimer = 0;
+    [SerializeField]
+    float spawnCooldown = 20;
+    float spawnTimer = 0;
 
-	[SerializeField]
-	int spawnLimit = 3;
+    [SerializeField]
+    int spawnIncStep = 4;
 
-	[SerializeField]
-	int spawnIncStep = 4;
+    [SerializeField]
+    Transform leftBase;
 
-	[SerializeField]
-	Transform leftBase;
+    [SerializeField]
+    Transform rightBase;
 
-	[SerializeField]
-	Transform rightBase;
+    int totalSpawned = 1;
 
-	int totalSpawned = 1;
+    // Use this for initialization
+    void Start()
+    {
+        if (spawnIncStep == 0)
+            spawnIncStep = 1;
 
-	// Use this for initialization
-	void Start () {
-		if (spawnIncStep == 0)
-			spawnIncStep = 1;
+        spawnTimer = Random.Range(0.5f, 1.0f) * spawnCooldown;
     }
-	
-        spawnTimer = Random.RandomRange(0.5f, 1.0f) * spawnCooldown;
-	// Update is called once per frame
-	void Update () {
-		spawnTimer -= Time.deltaTime;
-		if (spawnTimer < 0)
-		{
-            spawnTimer = Random.RandomRange(0.9f, 1.1f) * spawnCooldown;
-			SpawnTank();
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // don't keep him waiting
+        if (GameCore.Instance.Score >= totalSpawned && spawnTimer > 2.0f)
+            spawnTimer = 2.0f;
+
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer < 0)
+        {
+            spawnTimer = Random.Range(0.9f, 1.1f) * spawnCooldown / Mathf.Sqrt(GameCore.Instance.Score / spawnIncStep);
+            SpawnTank();
         }
-	}
+    }
 
-	void SpawnTank()
-	{
-		var pos = rightBase.transform.position;
-		if (GameCore.Instance.Score > 2 && Random.value < 0.5f)
-		{
-			pos = leftBase.transform.position;
-		}
+    //
+    void SpawnTank()
+    {
+        var pos = rightBase.transform.position;
+        if (GameCore.Instance.Score > 2 && Random.value < 0.5f)
+        {
+            pos = leftBase.transform.position;
+        }
 
-		if (totalSpawned - GameCore.Instance.Score >= 3 + GameCore.Instance.Score / spawnIncStep)
-			return;
+        if (totalSpawned - GameCore.Instance.Score >= 3 + GameCore.Instance.Score / spawnIncStep)
+            return;
 
-		pos.z = 0;
-		Instantiate(tankPrefab, pos, Quaternion.identity);
-		totalSpawned++;
+        pos.z = 0;
+        Instantiate(tankPrefab, pos, Quaternion.identity);
+        totalSpawned++;
     }
 }
